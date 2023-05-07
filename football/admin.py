6d693078f1a9018d0a58d98d7fb03253a18f89c0
Gaguchi <gaga9393@gmail.com>
2023-05-07 23:47:03 +0400

    admin.py update

--- football/admin.py
+++ football/admin.py
@@ -1,22 +1,44 @@
 from django.contrib import admin
-from .models import Competition, Team, Fixture, Player
+from .models import parentArea, Area, Competition, Season, Team, TeamCompetition, Fixture, Player
 
-@admin.register(Competition)
+
+class parentAreaAdmin(admin.ModelAdmin):
+    list_display = ('parentAreaId', 'parentArea')
+
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code', 'flag', 'parentArea')
+
+
 class CompetitionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    ordering = ('id',)
+    list_display = ('id', 'name', 'code', 'type', 'emblem', 'area', 'plan', 'current_season_id', 'last_updated')
 
-@admin.register(Team)
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start_date', 'end_date', 'current_matchday', 'winner')
+
+
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'short_name', 'tla', 'crest_url')
-    ordering = ('id',)
+    list_display = ('id', 'name', 'short_name', 'tla', 'crest_url', 'address', 'website', 'founded', 'club_colors', 'venue', 'last_updated')
 
-@admin.register(Fixture)
+
+class TeamCompetitionAdmin(admin.ModelAdmin):
+    list_display = ('team', 'competition', 'is_current')
+
+
 class FixtureAdmin(admin.ModelAdmin):
-    list_display = ('id', 'competition', 'home_team', 'away_team', 'status', 'matchday', 'season', 'kickoff_time', 'venue', 'referee')
-    ordering = ('id',)
+    list_display = ('id', 'competition', 'season', 'home_team', 'away_team', 'status', 'matchday', 'kickoff_time', 'venue', 'referee')
 
-@admin.register(Player)
+
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'position', 'nationality', 'team', 'appearances', 'goals', 'assists')
-    ordering = ('id',)
+
+
+admin.site.register(parentArea, parentAreaAdmin)
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(TeamCompetition, TeamCompetitionAdmin)
+admin.site.register(Fixture, FixtureAdmin)
+admin.site.register(Player, PlayerAdmin)
